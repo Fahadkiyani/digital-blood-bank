730ae62ecd3e75ff360e4c345007b2733981824a
Fahad kiyani <fahadkiyani08@gmail.com>
2020-05-21 15:12:42 +0500

    add .env file and connect DB. changes made in server.js,add posts and users pages in routes directory

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose =require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
+// Database connect
+mongoose.connect(process.env.db_connection_string,{useNewUrlParser:true},()=>console.log("DB is connected!"))
+
 // parse application/json
 app.use(bodyParser.json())
 // view engine embedded js
@@ -13,8 +18,9 @@ app.set('view engine', 'ejs');
 
 // root route
 app.get('/', (req,res)=>{
-    res.send('<h2>The server is up and running!</h2>')
+    res.send('')
 });
+
 
 
 const port = 3000;
