dc86c62535453bf738de360336a7c6f015d214f8
Fahad kiyani <fahadkiyani08@gmail.com>
2020-11-25 20:43:14 +0500

    down arrow added, hrtb flex grow and shrink is set, onload_msg.js is updated

--- public/js/onload_msg.js
+++ public/js/onload_msg.js
@@ -1,1 +1,17 @@
-window.onload(alert("Hello! \nThis website is under-development."));
+// window.onload(alert("Hello! \nThis website is under-development."));
+let downArrowBounce = document.querySelector('.fa-long-arrow-down');
+setTimeout(() => {
+        downArrowBounce.style.color = 'red';
+        downArrowBounce.style.animation
+}, 1000);
+
+
+window.onscroll = function() {scrollControll()};
+
+function scrollControll() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector('.arrow-container').style.visibility = 'hidden';
+  } else {
+    document.querySelector('.arrow-container').style.visibility = 'visible';
+  }
+}
